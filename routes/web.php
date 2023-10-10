cb29e515b4996fe8daae2b504418fcacaa735aac
RDhanuE <dhanuryvalino@gmail.com>
2023-10-10 13:25:49 +0700

    fix: bug fixed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+
+Route::resource('products', ProductController::class);
